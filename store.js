d3013475a83be12bcd037ed4fd9173799286a3a5
athreya18 <142571710+athreya18@users.noreply.github.com>
2024-02-28 16:48:21 +0530

    Create store.js
    Store.js inside a store folder defined for state management

---
+++ store.js
@@ -0,0 +1,31 @@
+import { create } from 'zustand'
+
+type TaskList = {
+    title: string,
+    description: string
+}
+export const useTaskList = create((set) => ({
+    createdTasks: [],
+    updateTask: (title: string, description: string) => {
+        set((state: any) => ({
+          createdTasks: [
+            ...state.createdTasks,
+            {
+              title: title,
+              description: description
+            } as TaskList,
+          ],
+        }));
+
+    },
+    deleteTask : (title: string) => {
+        set((state: any) => ({
+          createdTasks: state.createdTasks.filter((data: any) => {
+                data.title !== title
+          }), 
+        }));
+
+        
+    }
+   
+}))
