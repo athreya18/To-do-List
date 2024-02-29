9ada49d1f7a25f10387396526b83a4dc27ca3d8e
athreya18 <142571710+athreya18@users.noreply.github.com>
2024-02-29 16:55:56 +0530

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -4,12 +4,14 @@ import Image from "next/image"
 import iconimg from "../images/Group-1.svg";
 import john from "../images/john.svg";
 import list from "../images/list.svg";
+import newtask from "../images/newtask.svg"
 import ss from "../images/ss.svg";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger, } from "@/components/ui/sheet"
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button"
 import Todos from "@/components/ui/todo"
 import { useTaskList } from "../store/store";
+import { StateStorage } from 'zustand/middleware'
 
 
 
@@ -22,7 +24,8 @@ export default function Home() {
   
   const { updateTask, createdTasks}: any = useTaskList();
   const [isSheetOpen, setIsSheetOpen] = useState(false);
-  const [title, setTitle] = useState("")
+  const [tasks, setTasks] = useState<Array<{ title: string, desc: string }>>([]);
+  const [title, setTitle] = useState<string>("")
   const [desc, setDesc] = useState<string | undefined | null>("");
 
   const openSheet = () => {
@@ -41,29 +44,37 @@ export default function Home() {
     closeSheet();
 
   }
+  const createTask = (): void => {
+
+    setTasks([...tasks, { title , description }]);
+    setTitle('');
+    setDesc('');
+  };
   return (
     <>
     {/* header */}
-      <div className="pt-10 w-full h-16  flex flex-row justify-evenly align-center">
-        <div className=" w-20 h-7 flex flex-row justify-item-start align-center">
-          <Image src={iconimg} width={20} height={15} className=" pt-4 rounded-md" alt=""></Image>
-          <h1 className=" pt-2 text-2xl font-['Urbanist'] ">Taski</h1>
+      <div className=" p-10 pt-10 w-full h-16  flex flex-row justify-between align-center">
+        <div className="ml-20 w-20 h-7 flex flex-row justify-item-start align-center self-start">
+          <Image src={iconimg} width={20} height={15} className=" pt-4 self-start rounded-md" alt=""></Image>
+          <h1 className=" pt-2 text-2xl font-['Urbanist'] self-start">Taski</h1>
         </div>
-        <div className="flex flex-row w-24 h-10 align-center justify-content-center">
+        <div className="flex flex-row w-24 h-10 align-center justify-content-center mr-20">
           <h1 className=" pt-2 text-xl text-black ml-2 font-['Urbanist']">John</h1>
           <Image src={john} width={42} height={42} alt="" className="ml-2" />
         </div>
       </div>
       {/* Welcome John */}
+      
       <div className="mt-10 ml-20 w-48 h-9  flex flex-row justify-center items-center">
         <h1 className="ml-8 font-['Urbanist'] text-2xl font-bold leading-34 tracking-normal text-left"> Welcome,</h1>
-        <h1 className="text-blue-500 font-['Urbanist'] text-2xl font-bold leading-34 tracking-normal text-left"> John.</h1>
+        <h1 className="text-blue-500 font-['Urbanist'] text-2xl font-bold leading-34 tracking-normal text-left "> John.</h1>
       </div>
       <div className="w-56 h-5 ml-20 ">
         <p className=" ml-8 font-['Urbanist'] text-base font-medium leading-19 tracking-normal text-gray-500">Create tasks to achieve more.</p>
       </div>
+       
         {/* Image */}
-      {createdTasks.length == 0 && <div className="flex flex-row justify-center items-center">
+        {createdTasks.length == 0 && <div className="flex flex-row justify-center items-center">
         <Image src={list} width={148} height={144} alt="" className="mt-20"></Image>
       </div>}
       
@@ -89,7 +100,7 @@ export default function Home() {
                 </div>
                 <div className="flex flex-row justify-between align-center pt-8 w-full">
                   <Button variant="outline" className="w-[45%] h-12 font=['Urbanist'] p-5  rounded-12 border-1 border-solid  border-black gap-3 flex flex-row justify-center items-center " onClick={closeSheet}>Cancel</Button>
-                  <Button variant="outline" className="w-[45%] h-12 font=['Urbanist']  p-5 rounded-12 border-1 border-solid border-black gap-3 flex flex-row justify-center items-center bg-blue-500 bg-opacity-10 text-blue-600"onClick={closeSheet} onClick={update} >+ Create Task</Button>
+                  <Button variant="outline" className="w-[45%] h-12 font=['Urbanist']  p-5 rounded-12 border-1 border-solid border-black gap-3 flex flex-row justify-center items-center bg-blue-500 bg-opacity-10 text-blue-600" onClick={update} >+ Create Task</Button>
                 </div>
                 
               </SheetDescription>
@@ -98,7 +109,7 @@ export default function Home() {
         </Sheet>
 
       </div>
-      <Todos ></Todos>
+      <Todos tasks={createdTasks}/>
     </>
   );
 }
