bfc9e41db751630cb1e20388ad9ad96323600ade
athreya18 <142571710+athreya18@users.noreply.github.com>
2024-02-29 16:57:41 +0530

    Update store.ts
    Session storage is used

--- store.ts
+++ store.ts
@@ -1,32 +1,37 @@
-import { create } from 'zustand'
+import create from 'zustand';
+import { persist,createJSONStorage } from 'zustand/middleware';
 
 type TaskList = {
-    title: string,
-    description: string
-}
+  title: string;
+  description: string;
+};
 
-export const useTaskList = create((set) => ({
-    createdTasks: [],
-    updateTask: (title: string, description: string) => {
+export const useTaskList = create(
+  persist(
+    (set) => ({
+      createdTasks: [],
+      updateTask: (title: string, description: string) => {
         set((state: any) => ({
           createdTasks: [
             ...state.createdTasks,
             {
               title: title,
-              description: description
+              description: description,
             } as TaskList,
           ],
         }));
-
-    },
-    deleteTask : (title: string) => {
+      },
+      deleteTask: (title: string) => {
         set((state: any) => ({
-          createdTasks: state.createdTasks.filter((data: any) => {
-                data.title !== title
-          }), 
+          createdTasks: state.createdTasks.filter(
+            (data: any) => data.title !== title
+          ),
         }));
-
-        
+      },
+    }),
+    {
+      name: 'taskList', 
+      storage: createJSONStorage(() => sessionStorage),
     }
-   
-}))
+  )
+);
