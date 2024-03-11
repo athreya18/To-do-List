5299d7533c0dfb1b704f1405b7771c795c3c1463
athreya18 <142571710+athreya18@users.noreply.github.com>
2024-03-11 14:59:19 +0530

    Update todo.tsx

--- todo.tsx
+++ todo.tsx
@@ -3,38 +3,32 @@ import { useTaskList } from '@/store/store';
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-    DialogFooter,
-    DialogClose,
-} from "@/components/ui/dialog"
+import { Checkbox } from "@/components/ui/checkbox";
+import Image from "next/image";
+import editimg from "../images/edit.svg";
+import newtask from "../images/newtask.svg";
+import {Dialog,DialogContent,DialogDescription,DialogHeader,DialogTitle,DialogTrigger,DialogFooter,DialogClose,} from "@/components/ui/dialog"
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger, SheetDescription } from "@/components/ui/sheet";
 import ss from "../images/ss.svg"
+import deleteimg from "../images/delete.svg";
 import { close } from 'inspector';
 import { useEffect } from 'react';
 
 type TaskList = {
     title: string;
     description: string;
+    showEdit: boolean
 };
 
 const Todos = (props: any) => {
-    console.log(":::::::::::::::::::::::::::::::::::")
+    console.log(":::::::::::::::::::::::::::::::::::");
     const { updateTask, createdTasks, deleteTask, editTodoTasks }: any = useTaskList();
     const [isSheetOpen, setIsSheetOpen] = useState(false);
     const [tasks, setTasks] = useState<Array<{ title: string, desc: string }>>([]);
     const [selectedTaskIndex, setSelectedTaskIndex] = useState<number>(0);
-    const [title, setTitle] = useState<string>("")
+    const [title, setTitle] = useState<string>("");
     const [desc, setDesc] = useState<string>("");
-
-    useEffect(() => {
-        console.log({ createdTasks })
-    }, [createdTasks])
+    const [isCheckboxChecked, setIsCheckboxChecked] = useState<boolean>(false);
 
     const openSheet = () => {
         setIsSheetOpen(true);
@@ -48,8 +42,8 @@ const Todos = (props: any) => {
     };
 
     const update = () => {
-        debugger
-        editTodoTasks(title, desc, selectedTaskIndex);
+        console.log({title, desc, selectedTaskIndex})
+        editTodoTasks(title, desc, false, selectedTaskIndex);
         setSelectedTaskIndex(0);
         closeDialog();
     };
@@ -65,6 +59,7 @@ const Todos = (props: any) => {
         setSelectedTaskIndex(index);
         setTitle(createdTasks[index].title);
         setDesc(createdTasks[index].description);
+        // setIsSheetOpen(true);
         // setopenDialog(true);
         // openSheet();
     };
@@ -72,6 +67,8 @@ const Todos = (props: any) => {
     const closeDialog = () => {
         closeSheet();
     };
+
+    console.log({ createdTasks })
     return (
         <div>
             {createdTasks.length !== 0 && (
@@ -79,60 +76,65 @@ const Todos = (props: any) => {
                     <h3 className='pt-5 ml-20 font-bold flex flex-row justify-center items-center'>Created Tasks</h3>
                     <p className='ml-20 text-gray-500 flex flex-row justify-center items-center'>{`You have ${createdTasks.length} task${createdTasks.length !== 1 ? 's' : ''} to do`}</p>
                     <div className="flex flex-col items-center justify-center ">
-                        <Button className="flex flew-row self-center mr-20 mt-5" variant="outline" onClick={openSheet}>
-                            Add More Tasks
-                        </Button>
+                        <Image src={newtask} alt="" onClick={openSheet} width={182} height={10} className="ml-32 self-start flex flew-row hover:bg-rgba-121-136-164-1 " ></Image>
                         {createdTasks.length !== 0 && createdTasks.map((task: TaskList, index: number) => (
-                            <div key={index} className='mt-5 rounded-2xl bg-opacity-100 bg-[rgba(245,247,249,1)] flex flex-col justify-center items-center h-20 w-96 relative'>
-                                <div onClick={() => deleteTask(task.title)} className="absolute top-0 right-0 bg-red-500 text-white px-1 py-0.75 hover:bg-red-600 cursor-pointer">
-                                    X
-                                </div>
-                                <Dialog >
-                                    <DialogTrigger asChild>
-                                        <Button variant="outline" className="bg-blue-500 text-white px-1 py-0.5 rounded-md hover:bg-blue-600 mt-5 self-start" onClick={() => { editTasks(index) }}>
-                                            Edit
-                                        </Button>
-                                    </DialogTrigger>
-                                    <DialogContent className="sm:max-w-[425px]">
-                                        <DialogHeader>
-                                            <DialogTitle>Edit profile</DialogTitle>
-                                            <DialogDescription>
-                                                Make changes to your profile here. Click save when you're done.
-                                            </DialogDescription>
-                                        </DialogHeader>
-                                        <div className="grid gap-4 py-4">
-                                            <div className="grid grid-cols-4 items-center gap-4">
-                                                <Label htmlFor="title" className="text-right">
-                                                    Title
-                                                </Label>
-                                                <Input id="title" value={title} className="col-span-3" onChange={(e) => { setTitle(e.target.value) }} />
-                                            </div>
-                                            <div className="grid grid-cols-4 items-center gap-4">
-                                                <Label htmlFor="description" className="text-right">
-                                                    Description
-                                                </Label>
-                                                <Input id="description" value={desc} className="col-span-3" onChange={(e) => { setDesc(e.target.value) }} />
-                                            </div>
-                                        </div>
-                                        <DialogFooter>
-                                            <DialogClose>
-                                                <Button type="submit" onClick={update} >Save changes</Button>
-                                            </DialogClose>
-                                        </DialogFooter>
-                                    </DialogContent>
-                                </Dialog>
+                            <div key={index} className='mt-5 bg-[rgba(245,247,249,1)] bg-opacity-100 flex flex-col justify-center items-center w-3/4 h-28 rounded-2xl relative'>
+                                {task.showEdit && (
+                                    <>
+                                        {/* Delete */}
+                                        <Dialog>
+                                            <DialogTrigger>
+                                                <Image src={editimg} alt="" onClick={() => { editTasks(index) }} width={28} height={28} className="absolute top-10 right-12 cursor-pointer"></Image>
+                                            </DialogTrigger>
+                                            <DialogContent className="sm:max-w-[425px]">
+                                                <DialogHeader>
+                                                    <DialogTitle>Edit profile</DialogTitle>
+                                                    <DialogDescription>
+                                                        Make changes to your profile here. Click save when you're done.
+                                                    </DialogDescription>
+                                                </DialogHeader>
+                                                <div className="grid gap-4 py-4">
+                                                    <div className="grid grid-cols-4 items-center gap-4">
+                                                        <Label htmlFor="title" className="text-right">
+                                                            Title
+                                                        </Label>
+                                                        <Input id="title" value={title} className="col-span-3" onChange={(e) => { setTitle(e.target.value) }}  />
+                                                    </div>
+                                                    <div className="grid grid-cols-4 items-center gap-4">
+                                                        <Label htmlFor="desc" className="text-right">
+                                                            Description
+                                                        </Label>
+                                                        <Input id="desc" value={desc} className="col-span-3" onChange={(e) => { setDesc(e.target.value) }} />
+                                                    </div>
+                                                </div>
+                                                <DialogFooter>
+                                                    <Button type="submit" onClick={update}>Save changes</Button>
+                                                </DialogFooter>
+                                            </DialogContent>
 
-                                <div className=''>
-                                    <p className='pl-5 flex flex-row justify-center items-center font-bold'>Title: {task.title}</p>
-                                    <p className='pl-5 pb-7 flex flex-row justify-center items-center '>Description: {task.description}</p>
+                                        </Dialog>
+                                        <Image src={deleteimg} alt="" onClick={() => { deleteTask(task.title) }} width={28} height={28} className="absolute top-10 right-3 cursor-pointer"></Image>
+                                        {/* Edit */}
+                                    </>
+                                )}
+                                <div className='absolute flex flex-row justify-start self-start left-5 top-8'>
+                                    <div className="">
+                                        <Checkbox id={`checkbox-${index}`} checked={task.showEdit} onCheckedChange={() => {
+                                            editTodoTasks(task.title, task.description, !task.showEdit, index)
+                                            setIsCheckboxChecked(!isCheckboxChecked);
+                                        }} />
+                                        <Label htmlFor={`checkbox-${index}`}></Label>
+                                    </div>
                                 </div>
-                
+                                <div className='absolute left-20'>
+                                    <p className=' flex flex-row justify-center items-start font-bold '>{task.title}</p>
+                                    <p className=' pl-3 flex flex-row justify-center items-start text-rgba-141-156-184-1'>{task.description}</p>
+                                </div>
                             </div>
                         ))}
                     </div>
                 </>
             )}
-
             <Sheet open={isSheetOpen}>
                 <SheetContent>
                     <SheetHeader>
@@ -140,7 +142,6 @@ const Todos = (props: any) => {
                             <h2>Create Task</h2>
                             <h3 className="p-4 font=['Urbanist'] text-sm font-medium leading-17 tracking-normal text-left text-indigo-800 w-27 h-17">Title</h3>
                             <Input type="text" placeholder="Enter text.. " onChange={(e) => { setTitle(e.target.value) }} />
-
                             <h3 className=" mt-3 font=['Urbanist'] text-sm font-medium leading-17 tracking-normal text-left text-indigo-800 w-27 h-17">Description</h3>
                             <Input type="text" placeholder="Enter Description.." className="w-96 h-56" onChange={(e) => { setDesc(e.target.value) }} />
                         </SheetTitle>
@@ -155,7 +156,6 @@ const Todos = (props: any) => {
                                 <Button variant="outline" className="w-[45%] h-12 font=['Urbanist'] p-5  rounded-12 border-1 border-solid  border-black gap-3 flex flex-row justify-center items-center " onClick={closeSheet}>Cancel</Button>
                                 <Button variant="outline" className="w-[45%] h-12 font=['Urbanist']  p-5 rounded-12 border-1 border-solid border-black gap-3 flex flex-row justify-center items-center bg-blue-500 bg-opacity-10 text-blue-600" onClick={createTask} >+ Create Task</Button>
                             </div>
-
                         </SheetDescription>
                     </SheetHeader>
                 </SheetContent>
@@ -163,6 +163,4 @@ const Todos = (props: any) => {
         </div>
     );
 }
-
 export default Todos;
-
